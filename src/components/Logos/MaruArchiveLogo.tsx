487685f1a3c2778f70d6ecb2bf200dd967627abc
Rifqi Maulana <abelcaster025@gmail.com>
2024-07-21 21:40:52 +0700

    setup nextui, shadcn, og image, favicon, header, footer

---
+++ src/components/Logos/MaruArchiveLogo.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export const MaruArchiveLogo = () => (
+  <Image
+    src="/logos/maruarchive-light.png"
+    alt="maruarchive-light.png"
+    width={20}
+    height={20}
+  />
+);
